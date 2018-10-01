7c450368dedb151a5f8e0898b08a771268dc2da6
Gabrielly <ssgabi12@gmail.com>
2018-10-01 20:03:20 -0300

    Adicionando templates, navbar, footer e javascript; linkando agenda e blog

--- events/urls.py
+++ events/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='agenda-events-index'),
+    path('agenda', views.index, name='agenda-events-index'),
     path('all', views.all, name='agenda-events-all'),
     path('<int:id>', views.show, name='agenda-events-show'),
     path('<int:year>/<int:month>/<int:day>', views.day, name='agenda-events-day'),
